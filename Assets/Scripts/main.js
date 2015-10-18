32f8efac92d5bd3932141c6bb0a7e8e8fdbae140
Jadon Leininger <jleininger@student.neumont.edu>
2015-10-18 15:31:37 -0600

    Converted project to React.

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -1,8 +1,42 @@
-var map;
+var Map = React.createClass({
+    displayName: 'Map',
+    componentWillMount: function() {
+        this.loadScript();
+    },
+    componentDidMount: function() {
+        var scope = this;
+        setTimeout(function() {
+            scope.initialize();
+        }, 5000);
+    },
+    initialize: function() {
+        var mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(-34.397, 150.644)
+        };
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7660449, lng: -111.8910491},
-        zoom: 8
-    });
-}
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+    },
+    loadScript: function() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false';
+        document.body.appendChild(script);
+    },
+    render: function() {
+       return (
+           React.createElement('div', {id: 'map-canvas'}, null)
+       )
+    }
+});
+
+var Main = React.createClass({
+    render: function() {
+        return (
+            React.createElement(Map, null, null)
+        )
+    }
+});
+
+ReactDOM.render(React.createElement(Main, null, null), document.getElementById('container'));
