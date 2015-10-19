f8fe76058dc503605112fc1dd179f32d3b69fe86
Jadon Leininger <jleininger@student.neumont.edu>
2015-10-19 08:33:09 -0600

    Fixed issue with map not rendering with React.

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -1,27 +1,23 @@
+//Have to leave this outside React components for now to get map to show.
+function initialize() {
+    var mapOptions = {
+        zoom: 10,
+        center: new google.maps.LatLng(40.766088, -111.890743)
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+}
+
 var Map = React.createClass({
     displayName: 'Map',
     componentWillMount: function() {
         this.loadScript();
     },
-    componentDidMount: function() {
-        var scope = this;
-        setTimeout(function() {
-            scope.initialize();
-        }, 5000);
-    },
-    initialize: function() {
-        var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644)
-        };
-
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-    },
     loadScript: function() {
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false';
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&callback=initialize';
         document.body.appendChild(script);
     },
     render: function() {
