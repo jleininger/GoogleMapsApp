ba0bc4c07e538d8bd163381b5f72ba85f3fbebad
Jadon Leininger <jleininger@student.neumont.edu>
2015-10-19 09:03:33 -0600

    The map in now rendering like a real React app instead of the mess that it was before.

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -1,23 +1,45 @@
-//Have to leave this outside React components for now to get map to show.
-function initialize() {
-    var mapOptions = {
-        zoom: 10,
-        center: new google.maps.LatLng(40.766088, -111.890743)
-    };
-
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-}
-
 var Map = React.createClass({
     displayName: 'Map',
+    getInitialState: function() {
+        return {
+            mapOptions: {
+                zoom: 15,
+                center: null,
+                mapTypeId: null,
+                mapTypeControlOptions: null,
+                disableDoubleClickZoom: true,
+                scrollwheel: false,
+                draggableCursor: 'crosshair'
+            },
+            map: null,
+            path: null,
+            service: null
+        };
+    },
     componentWillMount: function() {
         this.loadScript();
     },
+    initialize: function() {
+        //Set up map options now that we have reference to google maps
+        var mapOptions = this.state.mapOptions;
+        mapOptions.center = new google.maps.LatLng(40.766088, -111.890743);
+        mapOptions.mapTypeId = google.maps.MapTypeId.HYBRID;
+        mapOptions.mapTypeControlOptions = {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.HYBRID,
+                google.maps.MapTypeId.SATELLITE]
+        };
+
+        this.state.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    },
     loadScript: function() {
+        var self = this;
+        window.initializeMap = function() {
+            self.initialize();
+        };
+
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&callback=initialize';
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&callback=initializeMap';
         document.body.appendChild(script);
     },
     render: function() {
